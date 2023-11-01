5cd1e19a002a09f53d96f182050d956433126aec
Brian Groenke <brian.groenke@awi.de>
2023-11-01 18:36:50 +0100

    Update tests for Turing ext

--- test/TuringExt/turing_tests.jl
+++ test/TuringExt/turing_tests.jl
@@ -18,12 +18,15 @@ end
 @testset "Turing priors" begin
     rng = Random.MersenneTwister(1234)
     m = testmodel1()
-    m_prior = TuringPrior(m)
-    draw = rand(m_prior)
+    prior = TuringPrior(m)
+    draw = rand(prior)
     @test isa(draw, ComponentVector)
     @test haskey(draw, :x) && haskey(draw, :p)
-    lp = logdensity(m_prior, draw)
-    @test lp == Turing.logprior(m_prior.model, (x=draw.x, p=draw.p))
-    chain = sample(rng, m_prior, 100, progress=false, verbose=false)
-    @test isa(chain, MCMCChains.Chains)
+    lp = logdensity(prior, draw)
+    @test lp == Turing.logprior(prior.model, (x=draw.x, p=draw.p))
+    draws = sample(rng, prior, 100, progress=false, verbose=false)
+    @test isa(draws, Vector{<:AbstractVector})
+    # test bijectors
+    b = bijector(prior)
+    @test length(b(draw)) == length(draw)
 end
