b62e54fcb0003c95edac3f3bd32e088b933e6aab
Brian Groenke <brian.groenke@awi.de>
2024-02-26 15:56:49 +0100

    Add DDM ode formulation and rearrange example files

--- examples/ddm/ddm.jl
+++ examples/ddm/ddm.jl
@@ -1,44 +1,48 @@
-"""
-    DDM(t,P,T,a,b)
+using ComponentArrays
+using Dates
+using Interpolations
+using Unitful
 
-this is my function....
 """
-DDM(t, P, T, a::Number, b::Number) = DDM(t, P, T, [a], [b])
-function DDM(t, P, T, a::AbstractVector, b::AbstractVector)
+    DDM(t, precip, Tair, a, b)
+
+Simple "degree-day" snow melt model with preciptation `precip` and air temperature
+`Tair` at time steps `t` as inputs. The parameters `a` and `b` correspond to the
+degree-day melt factor and precipitation scaling factor respectively. These parameters
+may be passed as vectors which would then correspond to an ensemble of values sampled
+from some prior distribution.
+"""
+DDM(t, precip, Tair, a::Number, b::Number; Tsnow=1.0, Tmelt=0.0) = DDM(t, precip, Tair, [a], [b]; Tsnow, Tmelt)
+function DDM(
+    t::AbstractVector{DateTime},
+    precip::AbstractVector{<:Real},
+    Tair::AbstractVector{<:Real},
+    a::AbstractVector{<:Number},
+    b::AbstractVector{<:Number};
+    Tsnow=1.0,
+    Tmelt=0.0
+)
     # DDM: A simple degree day snowmelt model
     # Coded for a single cell ensemble run, but could
     # also easily be extended to multiple cells stored as ensemble chunks.
     
-    # Hyperparameters:
-    Tsnow=1; # Temperature threshold for snowfall (degrees C)
-    Tmelt=0; # Temperature threshold for snowmelt (degrees C)
-    
     Nt=length(t); # Number of time steps.
     Ne=length(a); # Number of ensemble members (can be just 1).
     D_old=0; # Initial condition (no snow).
-    D=zeros(eltype(a), Nt,Ne);
+    D=zeros(eltype(a), Nt, Ne);
     
     b0=zeros(size(b));
     a0=zeros(size(a));
     
     for j=1:Nt
-        Pj=P[j];
-        Tj=T[j];
-        cansnow=Tj<=Tsnow; # Snow is possible.
+        Pj=precip[j];
+        Tj=Tair[j];
+        cansnow = Tj <= Tsnow; # Snow is possible.
         # For simplicity rain does not contribute to SWE
-        if cansnow
-            bj=b;
-        else
-            bj=b0;
-        end
-        Aj=Pj.*bj; # Accumulation for this day
+        bj = cansnow ? b : b0
         ddj=Tj-Tmelt; # Degree day for this day
-        melting=ddj>0;
-        if melting
-            aj=a;
-        else
-            aj=a0;
-        end
+        Aj=Pj.*bj; # Accumulation for this day
+        aj = ddj > zero(ddj) ? a : a0
         Mj=ddj.*aj;
         D_new=D_old.+Aj.-Mj;
         D_new=max.(D_new,0);
@@ -47,4 +51,59 @@ function DDM(t, P, T, a::AbstractVector, b::AbstractVector)
     end
 
     return D
+end
+
+"""
+    DDM_ode(t, precip, Tair)
+
+Same basic model as `DDM` but defines the problem as a continuous ODE with non-autonomous inputs
+`precip` and `Tair`. Returns a `NamedTuple` of the form: `(; dudt, resid, u0, p)` where `dudt`
+is the flux (derivative of the state at time `t`), `resid` is a residual function that is nonzero
+when the state is outside of the physical boudns (i.e. negative), `u0` is the initial state, and `p`
+are the parameters of the system.
+"""
+function DDM_ode(
+    t::AbstractVector{DateTime},
+    precip::AbstractVector{<:Real},
+    Tair::AbstractVector{<:Real},
+)
+    t_real = ustrip.(u"s", float.(Dates.datetime2epochms.(t)).*u"ms")
+    tspan = extrema(t_real)
+    # linear interpolation for Tair
+    Tair_interp = interpolate((t_real,), Tair, Gridded(Linear()))
+    # constant interpolation for precipitation
+    precip_interp = interpolate((t_real,), precip./(24*3600), Gridded(Constant()))
+    # set up parameter vector
+    p = ComponentVector(
+        a = 1.0,
+        b = 1.0,
+        Tsnow = 1.0,
+        Tmelt = 0.0,
+    )
+
+    # dudt function defining the ODE
+    DDM_dudt(u, p, t::DateTime) = DDM_dudt(u, p, Dates.datetime2epochms(t))
+    function DDM_dudt(u, p, t::Number)
+        # unpack parameters
+        a, b, Tsnow, Tmelt = p
+        # get current forcing values
+        Tair_t = Tair_interp(t)
+        precip_t = precip_interp(t)
+        # adjust parameters based on temperature
+        b = ifelse(Tair_t <= Tsnow, b, zero(b))
+        a = ifelse(Tair_t > Tmelt, a, zero(a))
+        # accumulation
+        acc = precip_t*b
+        # compute snow melt flux
+        dd = ifelse(u[1] > zero(eltype(u)), Tair_t - Tmelt, zero(eltype(u)))
+        dudt = acc - dd*a./(24*3600.0)
+        return zeros(size(u)) .+ dudt
+    end
+
+    # residual function which is nonzero when the state is non-negative
+    function DDM_resid(u, p)
+        return min.(u, zero(eltype(u)))
+    end
+
+    return (dudt=DDM_dudt, resid=DDM_resid, u0=[0.0], p=p, tspan=tspan)
 end
--- examples/ddm/run_ddm.jl
+++ examples/ddm/run_ddm.jl
@@ -2,27 +2,51 @@ using Downloads: download
 using MAT
 using Plots
 
-const URL = "https://www.dropbox.com/scl/fi/fbxn7antmrchk39li44l6/daily_forcing.mat?rlkey=u1s2lu13f4grqnbxt4ediwlk2&dl=0"
+const download_url = "https://www.dropbox.com/scl/fi/fbxn7antmrchk39li44l6/daily_forcing.mat?rlkey=u1s2lu13f4grqnbxt4ediwlk2&dl=0"
 
-include("../datenum.jl")
+include("datenum.jl")
 include("ddm.jl")
 
-filepath = download(URL, joinpath("examples", "data", "finse_tp.mat"))
+datadir = mkpath(joinpath("examples", "data"))
+filepath = joinpath(datadir, "finse_tp.mat")
+if !isfile(filepath)
+    @info "Downloading forcing data to $filepath"
+    download(download_url, filepath)
+end
+
 data = matread(filepath)
 forcing = data["f"]
-ts = todatetime.(DateNumber.(forcing["t"]))
-P=forcing["P"]
-T=forcing["T"]
+ts = todatetime.(DateNumber.(forcing["t"]))[:,1]
+precip = forcing["P"][:,1]
+Tair = forcing["T"][:,1]
 
-a=[3.0,10.0]
-b=[1.0,5.0]
+a = [1.0,3.0,10.0]
+b = [1.0,1.0,5.0]
 
+# uncomment to debug in VSCode
 #@run D=DDM(ts,P,T,a,b)
-D=DDM(ts,P,T,a,b)
+
+D = @time DDM(ts, precip, Tair, a, b)
+
 plot(D)
 
-using ForwardDiff
+# define the system as an ODE instead;
+# there probably isn't any real benefit to this since the simple DDM model
+# is autonomous w.r.t the state; i.e. only the forcings actually change the system.
+using OrdinaryDiffEq
+using DiffEqCallbacks
 
-loss(p) = mean(DDM(ts,P,T,p[1],p[2]))
+dudt, resid, u0, p, tspan = DDM_ode(ts, precip, Tair);
+# define ODEProblem with "positive domain" callback
+prob = ODEProblem(dudt, u0, tspan, p, callback=PositiveDomain(u0))
+sol = @time solve(prob, Heun(), dtmax=24*3600.0)
+D_sol = reduce(vcat, sol.(tspan[1]:24*3600:tspan[end]))
+plot([D_sol D[:,1]])
+
+# taking gradients using forward-mode AD
+using ForwardDiff
+using Statistics
+
+loss(p) = mean(DDM(ts, precip, Tair, p[1], p[2]))
 
 ForwardDiff.gradient(loss, [3.0,1.0])
