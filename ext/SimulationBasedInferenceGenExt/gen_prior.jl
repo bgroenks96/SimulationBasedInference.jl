e7875784a4888f61ef515b70673c1c06a9c8e08b
Brian Groenke <brian.groenke@awi.de>
2024-07-11 16:58:03 +0200

    Add support for parameter subsets in Gen prior

--- ext/SimulationBasedInferenceGenExt/gen_prior.jl
+++ ext/SimulationBasedInferenceGenExt/gen_prior.jl
@@ -1,42 +1,56 @@
 """
-    GenSimulatorPrior{genType<:GenerativeFunction,argsType,axesType} <: AbstractSimulatorPrior
+    GenSimulatorPrior{genType<:GenerativeFunction,argsType,selType,axesType,paraTypes,staticTypes} <: AbstractSimulatorPrior
 
-Represents a prior distribution formulated as a `Turing` model. The Turing model
-can have any arbitrary structure, e.g. hierarchical or otherwise.
+Represents a prior distribution formulated as a `Gen` stochastic function.
 """
-struct GenSimulatorPrior{genType<:Gen.GenerativeFunction,argsType,choiceMapType,axesType} <: AbstractSimulatorPrior
+struct GenSimulatorPrior{genType<:Gen.GenerativeFunction,argsType,selType,axesType,paraTypes,staticTypes} <: AbstractSimulatorPrior
     gf::genType
     args::argsType
     axes::axesType
     names::Vector{Symbol}
-    choicemap_proto::choiceMapType # necessary for Gen.from_array
+    sel::selType
+    param_choices::paraTypes
+    static_choices::staticTypes
 end
 
-function GenSimulatorPrior(gf::Gen.GenerativeFunction, args::Tuple)
-    trace = simulate(gf, args)
-    choices = to_static_choice_map(choicemap(get_choices(trace)))
+function GenSimulatorPrior(
+    gf::Gen.GenerativeFunction,
+    args::Tuple,
+    sel::Selection=selectall(),
+    preset_choices::ChoiceMap=choicemap(),
+)
+    trace, _ = generate(gf, args, preset_choices)
+    choices = to_static_choice_map(get_selected(choicemap(get_choices(trace)), sel))
     choices_nt = choicemap2nt(choices)
     datatype = isempty(choices_nt) ? Float64 : eltype(choices_nt)
     params = ComponentVector{datatype}(choices_nt)
     names = Symbol.(labels(params))
     # use zero values for prototype choice map
-    choices_proto = Gen.from_array(choices, getdata(zero(params)))
-    return GenSimulatorPrior(gf, args, getaxes(params), names, choices_proto)
+    para_choices = Gen.from_array(choices, getdata(zero(params)))
+    static_choices = to_static_choice_map(preset_choices)
+    return GenSimulatorPrior(gf, args, getaxes(params), names, sel, para_choices, static_choices)
 end
 
 function (prior::GenSimulatorPrior)(θ::AbstractVector{T}) where {T}
-    choices = Gen.from_array(prior.choicemap_proto, Vector(θ))
-    trace, _ = generate(prior.gf, prior.args, choices)
+    # create choice map from parameter array
+    choices = Gen.from_array(prior.param_choices, Vector(θ))
+    # merge with conditioned static choices
+    trace, _ = generate(prior.gf, prior.args, merge(prior.static_choices, choices))
     return Gen.get_retval(trace)
 end
 
-SimulationBasedInference.prior(model::Gen.GenerativeFunction, args::Tuple) = GenSimulatorPrior(model, args)
+SimulationBasedInference.prior(
+    model::Gen.GenerativeFunction,
+    args::Tuple,
+    sel::Selection=Gen.selectall(),
+    choices::ChoiceMap=choicemap(),
+) = GenSimulatorPrior(model, args, sel, choices)
 
 SimulationBasedInference.forward_map(prior::GenSimulatorPrior, θ::AbstractVector) = prior(θ)
 
 SimulationBasedInference.logprob(prior::GenSimulatorPrior, θ::NamedTuple) = logprob(prior.model, ComponentVector(θ))
 function SimulationBasedInference.logprob(prior::GenSimulatorPrior, θ::AbstractVector)
-    choices = Gen.from_array(prior.choicemap_proto, Vector(θ))
+    choices = Gen.from_array(prior.param_choices, Vector(θ))
     weight, retval = assess(prior.gf, prior.args, choices)
     return weight
 end
@@ -45,8 +59,8 @@ end
 function Base.rand(rng::AbstractRNG, prior::GenSimulatorPrior)
     # TODO: use RNG once PR is merged
     # https://github.com/probcomp/Gen.jl/pull/520
-    trace = simulate(prior.gf, prior.args)
-    choices = to_static_choice_map(choicemap(get_choices(trace)))
+    trace, _ = generate(prior.gf, prior.args, prior.static_choices)
+    choices = to_static_choice_map(get_selected(choicemap(get_choices(trace)), prior.sel))
     params = ComponentVector(choicemap2nt(choices))
     return params
 end
